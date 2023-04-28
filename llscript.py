4f0d16bd983e409f4abae901b593673bbdd0ad23
PedroZaffalon <pedrozaffalondasilva@gmail.com>
2023-04-27 21:36:16 -0300

    adicionado click no llscript

--- llscript.py
+++ llscript.py
@@ -1,49 +1,47 @@
 import os
 import subprocess
-import sys
+import click
 
-def generatell(dir_path, output_dir):
+@click.command()
+@click.option('--dir', '-d', default="", help='Path to directory with .ll input files.')
+@click.option('--output', '-o', default="", help='Path to output directory.')
+
+def cli(dir, output):
+
+    if dir == "":
+        dir = os.getcwd()
+    
+    if output == "":
+        output = dir
+    
     # Primeiro, definimos a linha de comando que será executada para cada arquivo .c
     c_command = "clang -Xclang -disable-O0-optnone -S -emit-llvm {} -o {}.ll"
 
     # Em seguida, iteramos sobre todos os arquivos .c no diretório
-    for file_name in os.listdir(dir_path):
+    for file_name in os.listdir(dir):
         if file_name.endswith(".c"):
             # Executamos a linha de comando para cada arquivo .c
-            input_file_name = os.path.join(dir_path, file_name)
+            input_file_name = os.path.join(dir, file_name)
             command = c_command.format(input_file_name, input_file_name[:-2])
             subprocess.run(command, shell=True)
     
     # Certificamo-nos de que o diretório de saída existe; se não, criamos um
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    if not os.path.exists(output):
+        os.makedirs(output)
 
     # Definimos a linha de comando para gerar os arquivos .ll de saída
     ll_command = "opt -S -mem2reg {}.ll -o {}.ll"
 
     # Iteramos sobre todos os arquivos .ll gerados pelo comando anterior
-    for file_name in os.listdir(dir_path):
+    for file_name in os.listdir(dir):
         if file_name.endswith(".ll"):
             # Executamos a linha de comando para cada arquivo .ll
-            input_file_name = os.path.join(dir_path, file_name[:-3])
-            output_file_name = os.path.join(output_dir, file_name[:-3])
+            input_file_name = os.path.join(dir, file_name[:-3])
+            output_file_name = os.path.join(output, file_name[:-3])
             command = ll_command.format(input_file_name, output_file_name)
             subprocess.run(command, shell=True)
 
+    
+
 if __name__ == '__main__':
-    args_count = len(sys.argv)
-    if args_count == 1:
-        dir_path = os.getcwd()
-        output_dir = dir_path
-    elif args_count == 2:
-        dir_path = os.getcwd()
-        output_dir = sys.argv[1]
-    elif args_count == 3:
-        dir_path = sys.argv[1]
-        output_dir = sys.argv[2]
-    else:
-        print("Error\n")
-        quit(1)
-    
-    generatell(dir_path, output_dir)
-    
+    cli()
